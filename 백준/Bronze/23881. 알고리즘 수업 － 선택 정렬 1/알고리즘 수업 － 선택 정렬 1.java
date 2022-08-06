6dc55be703494a9e8e68054bb8f6cc52b0559f82
Water-Stone <67692815+Water-Stone@users.noreply.github.com>
2022-08-06 16:20:12 +0900

    [Bronze I] Title: 알고리즘 수업 - 선택 정렬 1, Time: 256 ms, Memory: 17860 KB -BaekjoonHub

---
+++ 백준/Bronze/23881. 알고리즘 수업 － 선택 정렬 1/알고리즘 수업 － 선택 정렬 1.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        br.close();
+
+        int cnt = 0;
+        for (int i = N - 1; i > 0; i--) {
+            int max = arr[i], maxIdx = i;
+            for (int j = i - 1; j >= 0; j--) {
+                if (arr[j] > max) {
+                    max = arr[j];
+                    maxIdx = j;
+                }
+            }
+            if (arr[i] < max) {
+                int tmp = arr[i];
+                arr[i] = max;
+                arr[maxIdx] = tmp;
+                cnt++;
+                if (cnt == K) {
+                    sb.append(tmp).append(" ").append(arr[i]);
+                    break;
+                }
+            }
+        }
+
+        if (cnt < K)
+            sb.append(-1);
+
+        System.out.println(sb);
+    }
+}
