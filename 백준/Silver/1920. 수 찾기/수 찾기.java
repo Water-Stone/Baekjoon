f2ebe4880c73a6ef5aa2b06aab6e0b62c7302fdc
Water-Stone <67692815+Water-Stone@users.noreply.github.com>
2022-08-09 15:25:10 +0900

    [Silver IV] Title: 수 찾기, Time: 748 ms, Memory: 42904 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -0,0 +1,49 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        int[] numbers = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(numbers);
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            int x = Integer.parseInt(st.nextToken());
+
+            if (binarySearch(numbers, x))
+                sb.append(1);
+            else sb.append(0);
+            sb.append("\n");
+        }
+        System.out.print(sb);
+        br.close();
+    }
+
+    private static boolean binarySearch(int[] numbers, int x) {
+        int lo = 0;
+        int hi = numbers.length - 1;
+        int mid = (lo + hi) / 2;
+
+        while (lo < hi) {
+            mid = (lo + hi) / 2;
+
+            if (x < numbers[mid]) {
+                hi = mid - 1;
+            } else if (x > numbers[mid]) {
+                lo = mid + 1;
+            } else {
+                return true;
+            }
+        }
+        return (x == numbers[lo]);
+    }
+}
