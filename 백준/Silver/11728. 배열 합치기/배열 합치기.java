d07613030c7261022c581fbf057d73d1c9d9319e
Water-Stone <67692815+Water-Stone@users.noreply.github.com>
2022-08-12 15:47:56 +0900

    [Silver V] Title: 배열 합치기, Time: 1504 ms, Memory: 344868 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.java
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.java
@@ -4,29 +4,39 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-//        List<Integer> list = new ArrayList<>(N+M);
-        PriorityQueue<Integer> list = new PriorityQueue<>();
+        List<Integer> list = new ArrayList<>(N + M);
+        int[] A = new int[N];
+        int[] B = new int[M];
 
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++)
-            list.add(Integer.parseInt(st.nextToken()));
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
 
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < M; i++)
-            list.add(Integer.parseInt(st.nextToken()));
+        for (int i = 0; i < M; i++) {
+            B[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int i = 0, j = 0;
+        while (i < N && j < M) {
+            if (A[i] > B[j]) list.add(B[j++]);
+            else list.add(A[i++]);
+        }
+
+        while (i < N) list.add(A[i++]);
+        while (j < M) list.add(B[j++]);
+
+        for (int n : list)
+            sb.append(n).append(" ");
 
         br.close();
-
-        StringBuilder sb = new StringBuilder();
-
-        while (!list.isEmpty())
-            sb.append(list.poll()).append(" ");
-
         System.out.println(sb);
     }
 }
