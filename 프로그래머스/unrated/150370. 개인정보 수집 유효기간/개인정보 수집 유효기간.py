dacb39220128c4cd02e495ee8239b26609212f80
Water-Stone <67692815+Water-Stone@users.noreply.github.com>
2023-05-03 17:32:34 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 0.54 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -6,20 +6,10 @@ def diff_date(today, past):
     py = int(past.split('.')[0])
     pm = int(past.split('.')[1])
     pd = int(past.split('.')[2])
-    # year_diff = ty - py
-    # mon_diff = tm - pm
-    # day_diff = td - pd
     
     # 2000.01.01 기준 날 수
     t_parsed = ((ty - 2000) * 12 * 28) + ((tm - 1) * 28) + td
     p_parsed = ((py - 2000) * 12 * 28) + ((pm - 1) * 28) + pd
-    
-    # if year_diff == 0:
-    #     if mon_diff == 0:
-    #         return day_diff
-    #     return (28 - pd) +\
-    #         td + (28 * mon_diff)
-    # return (28 - pd) + (28 * (11 - pm) + tm) + td
     
     return t_parsed - p_parsed
 
