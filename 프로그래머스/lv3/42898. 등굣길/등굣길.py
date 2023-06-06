692930950d2d152a96da7bb6c7f2d32b10779c4b
Water-Stone <67692815+Water-Stone@users.noreply.github.com>
2023-06-06 15:22:29 +0900

    [level 3] Title: 등굣길, Time: 2.22 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42898. 등굣길/등굣길.py
@@ -0,0 +1,12 @@
+def solution(m, n, puddles):
+    dp = [[0 for col in range(m + 1)] for row in range(n + 1)]
+    # home = dp[1][1]
+    dp[1][1] = 1
+    
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            if (i == 1 and j == 1) or ([j, i] in puddles):
+                continue
+            dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+    
+    return dp[n][m] % 1000000007
