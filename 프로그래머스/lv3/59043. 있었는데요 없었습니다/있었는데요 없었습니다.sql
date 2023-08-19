f1eb22e2b57928c0b57c8ba5c269e54cb4c95249
Water-Stone <67692815+Water-Stone@users.noreply.github.com>
2023-08-20 00:51:28 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -0,0 +1,10 @@
+# 보호 시작일보다 입양일이 더 빠른 동물의 아이디와 이름을 조회하는 SQL문을 작성해주세요.
+# 이때 결과는 보호 시작일이 빠른 순으로 조회해야합니다.
+SELECT
+  OUTS.ANIMAL_ID,
+  OUTS.NAME
+FROM ANIMAL_INS AS INS
+JOIN ANIMAL_OUTS AS OUTS
+  ON INS.ANIMAL_ID = OUTS.ANIMAL_ID
+WHERE INS.DATETIME > OUTS.DATETIME
+ORDER BY INS.DATETIME
