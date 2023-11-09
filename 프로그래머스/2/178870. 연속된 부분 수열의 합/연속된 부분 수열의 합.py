0f8fabd3c4b2337f6ef5528496332d1a756a07b7
Water-Stone <67692815+Water-Stone@users.noreply.github.com>
2023-11-10 01:27:59 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
@@ -0,0 +1,19 @@
+def solution(sequence, k):
+    answer = []
+    summ = 0
+    last_index = len(sequence)-1
+    for i, v in enumerate(sequence[::-1]):
+        summ += v
+        if summ < k:
+            pass
+        elif summ > k:
+            summ -= sequence[last_index]
+            last_index -= 1
+            if summ == k:
+                answer.append([len(sequence)-1-i, last_index])
+
+        else:
+            answer.append([len(sequence)-1-i, last_index])
+
+    answer.sort(key=lambda x : (x[1]-x[0], x[0]))
+    return answer[0]
